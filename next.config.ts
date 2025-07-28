97e01feb3470481a95835018e89948f3b73cae00
Heriberto Perez <heribertoperez04@gmail.com>
2025-07-28 18:55:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -19,25 +19,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devServer: {
-    // Add this to fix the Next.js server constantly restarting in the container.
-    // https://nextjs.org/docs/app/api-reference/next-config-js/server-and-client-files-resolution#serverfiles-in-nextconfigjs
-    //
-    // "When this option is enabled, Next.js will not restart the server when
-    // any of these files are changed. This is useful for Docker containers
-    // where the file system can be slow to update."
-    experimental: {
-      serverActions: {
-        // Since we are using a mono-repo, we need to allow all files to be
-        // loaded by the server.
-        allowedForwardedHosts: ['localhost'],
-      },
-      // This is necessary to prevent the server from restarting when files are
-      // changed in the container.
-      serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  experimental: {
+    serverActions: {
+      // Since we are using a mono-repo, we need to allow all files to be
+      // loaded by the server.
+      allowedForwardedHosts: ['localhost'],
     },
-    allowedDevOrigins: ["*.cloudworkstations.dev"],
-  }
+    // This is necessary to prevent the server from restarting when files are
+    // changed in the container.
+    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
 };
 
 export default nextConfig;
